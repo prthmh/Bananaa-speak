0055ed2c7ebb057b78ba0bbe293a79ccd3fadebc
Prathmesh <pratmbr@gmail.com>
2022-10-31 22:43:57 +0530

    Update new.js

--- new.js
+++ new.js
@@ -18,4 +18,14 @@ function errorHandler(error) {
 function clickHandler() {
     var inputText = txtInput.value; // taking input
 
-    
+     // calling server for processing
+     fetch(getTranslationURL(inputText))
+     .then(response => response.json())
+     .then(json => {
+         var translatedText = json.contents.translated;
+         outputDiv.innerText = translatedText; // output
+        })
+     .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
